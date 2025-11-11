6432b94709e352daaa3ec7de3f22074a5ad84958
suryadedatta <surya.dedatta@menloschool.org>
2025-11-10 21:04:52 -0800

    Finished Program

--- src/Finder.java
+++ src/Finder.java
@@ -14,59 +14,29 @@ import java.util.ArrayList;
 public class Finder {
 
     private static final String INVALID = "INVALID KEY";
-    public static final long P = 2305843009213693951L;
-    ArrayList<String[]> arr;
+    HashMap map;
     public Finder()
     {
-        arr = new ArrayList<>();
+        map = new HashMap();
     }
 
-    public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
-        // TODO: Complete the buildTable() function!
-        //Array of ArrayLists
-        int lineCount = 0;
-        int arrCounter = 0;//
+    public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException
+    {
         String line = br.readLine();
+        // Reads through the buffered read object line by line
         while(line != null)
         {
+            // Divides each line into it's key and value
             String[] words = line.split(",");
-            String str1 = words[0];
-            String str2 = words[1];
-            lineCount++;
-            String[] inner = new String[2];
-            int index = hash(str1);
-            inner[0] = str1;
-            inner[1] = str2;
-            arr.add(index, inner);
-            arrCounter++;
+            map.add(words[keyCol],words[valCol]);
+            line = br.readLine();
         }
-        // Hash function that can calculate a unique index for an arraylist
-        // Array of Arraylists that each hold a key and a value;
         br.close();
-    }
-    public static int hash(String t)
-    {
-        int m = t.length();
-        long h = 0;
-        for(int i = 0; i < m; i++)
-        {
-            // Shift previous hash value to make room for the new character and adds current character
-            h = (h * RADIX + t.charAt(i)) %  P;
-        }
-        return (int) h;
     }
     public String query(String key)
     {
-        // TODO: Complete the query() function!
-        int val = hash(key);
-        String value = "";
-        if(val >= 0 && val < arr.size())
-        {
-            if(arr.get(val) != null)
-            {
-               value = arr.get(val)[(1)];
-            }
-        }
+        // Ensures a value is present for a given key using map
+        String value = map.get(key);
         if(value != null)
         {
             return value;
--- src/HashMap.java
+++ src/HashMap.java
@@ -1,17 +1,21 @@
+import java.util.Objects;
+
 public class HashMap
 {
     public static final long RADIX = 256;
-    public static final int DEFAULT_TABLE_SIZE = 2;
+    // Creates a reasonable initial table size based on resizing ability and max data set size
+    public static final int DEFAULT_TABLE_SIZE = 997;
     int tableSize;
     int n;
     String[] keys;
     String[] values;
 
-    public HashMap(int numElements)
+    public HashMap()
     {
+        n = 0;
         tableSize = DEFAULT_TABLE_SIZE;
-        this.keys = new String[n];
-        this.values = new String[n];
+        this.keys = new String[DEFAULT_TABLE_SIZE];
+        this.values = new String[DEFAULT_TABLE_SIZE];
     }
     public int hash(String key)
     {
@@ -20,26 +24,60 @@ public class HashMap
         for(int i = 0; i < m; i++)
         {
             // Shift previous hash value to make room for the new character and adds current character
-            h = (int) ((h * RADIX + key.charAt(i)) %  (tableSize*2 -1));
+            h = (int) ((h * RADIX + key.charAt(i)) %  tableSize);
         }
         return h;
     }
-    void add(String key, String value)
+    public void add(String key, String value)
     {
-
+        // Ensures load factor is below 0.5 to avoid long linear probing sequences
+        if(n > 0.5 * tableSize)
+        {
+            resize();
+        }
+        int index = hash(key);
+        while(keys[index] != null)
+        {
+            // Moves 1 index over and wraps around to beginning of map in case of collision
+            index = (index + 1) % tableSize;
+        }
+        keys[index] = key;
+        values[index] = value;
+        // Keeps track of number of elements currently in the map
+        n++;
     }
-    String get(String key)
+    public String get(String key)
     {
         int val = hash(key);
-        int trueVal = val;
-        if(values[val] == )
+        int index = val;
+        while(keys[index] != null)
         {
-
+            // Finds the proper index for a given key and returns its value
+            if(keys[index].equals(key))
+            {
+                return values[index];
+            }
+            index = (index + 1) % tableSize;
         }
+       return null;
     }
+    // Increases map size to avoid long linear probing chains
     public void resize()
     {
-        tableSize *= 2;
+        // Resizes 1 more than double to keep table size odd to avoid collisions
+        tableSize *= 2 + 1;
+        String oldKeys[] = keys;
+        String oldValues[] = values;
+        keys = new String[tableSize];
+        values = new String[tableSize];
+        for (int i = 0; i < oldKeys.length; i++)
+        {
+            if(oldKeys[i] != null)
+            {
+                // Adds old values back to newly sized keys and values arrays
+                add(oldKeys[i], oldValues[i]);
+            }
+        }
     }
 
 }
